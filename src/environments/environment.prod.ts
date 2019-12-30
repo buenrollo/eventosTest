ea76b0c94ea814b31e44a83aab364ce60b01f224
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2019-12-30 19:02:37 +0100

    Version 0.0.1

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  test: false,
-  appUrlBase: "https://buenrollo.github.io/quedadas/",
-  firebaseUrls: {
-    buenrollo: "https://bdbuenrollo.firebaseio.com/"
-  }
-};
